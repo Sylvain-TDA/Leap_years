3a05cfb64a80b0f123e67ca8fd65d14ffb3e444f
Sylvain-TDA <sylvain.teissier@le-campus-numerique.fr>
2025-09-18 15:31:52 +0200

    feat:fizz buzz

---
+++ FizzBuzz/fizz_buzz.py
@@ -0,0 +1,18 @@
+def fizz():
+    fizzbuzz_list = ""
+    i = 1
+
+    while i < 100:
+        if i % 3 == 0 and i % 5 == 0:
+            fizzbuzz_list += "FizzBuzz "
+        elif i % 5 == 0:
+            fizzbuzz_list += "Buzz "
+        elif i % 3 == 0:
+            fizzbuzz_list += "Fizz "
+        else:
+            fizzbuzz_list += f"{i}"
+        i += 1
+    return fizzbuzz_list
+
+d = fizz()
+print(d)
---
+++ FizzBuzz/test_fizz_buzz.py
@@ -0,0 +1,4 @@
+from fizz_buzz import *
+
+def test_div_by_tree():
+    assert fizz() == "12Fizz 4Buzz Fizz 78Fizz Buzz 11Fizz 1314FizzBuzz 1617Fizz 19Buzz Fizz 2223Fizz Buzz 26Fizz 2829FizzBuzz 3132Fizz 34Buzz Fizz 3738Fizz Buzz 41Fizz 4344FizzBuzz 4647Fizz 49Buzz Fizz 5253Fizz Buzz 56Fizz 5859FizzBuzz 6162Fizz 64Buzz Fizz 6768Fizz Buzz 71Fizz 7374FizzBuzz 7677Fizz 79Buzz Fizz 8283Fizz Buzz 86Fizz 8889FizzBuzz 9192Fizz 94Buzz Fizz 9798Fizz "
